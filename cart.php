fb3719345c90dd8f350da33338d3e872f2f564d3
hanhut0710 <huynhanhnhut94@gmail.com>
2025-03-16 12:56:27 +0700

    ss

---
+++ cart.php
@@ -0,0 +1,5 @@
+<?php include 'layout/header.php'; ?>
+<?php include 'layout/navbar.php'; ?>
+<?php include 'layout/cart_home_section.php'; ?>
+<?php include 'layout/cart_main.php'; ?>
+<?php include 'layout/footer.php'; ?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include 'layout/header.php'; ?>
+<?php include 'layout/navbar.php'; ?>
+<?php include 'layout/home_section.php'; ?>
+<?php include 'layout/products.php' ?>
+<?php include 'layout/footer.php'; ?>
