979b187e13783df8c38421eb480920decc439ef0
hanhut0710 <huynhanhnhut94@gmail.com>
2025-03-22 09:35:24 +0700

    chia layout

--- shop.php
+++ shop.php
@@ -25,9 +25,12 @@
 </head>
 
 <body>
+    
     <?php include("./layout/header.php");?>
     <?php include("./layout/products.php");?>
     <?php include("./layout/footer.php");?>
+
+
     <script src="js/jquery-3.3.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/jquery.nice-select.min.js"></script>
