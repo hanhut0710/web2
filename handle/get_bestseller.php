49401ea1caee454b42bb02088771bc5cf546d90a
hanhut0710 <huynhanhnhut94@gmail.com>
2025-03-22 10:23:43 +0700

    ss

---
+++ handle/get_bestseller.php
@@ -0,0 +1,22 @@
+<?php 
+    include("./connect.php");
+
+    $sql = "SELECT id, name, price, img_src FROM products
+            ORDER BY price DESC
+            LIMIT 5";
+
+    $result = $con->query($sql);
+
+    $data = array();
+
+    if($result->num_rows > 0){
+        while($row = $result->fetch_assoc()){
+            $data[] = $row;
+        }
+    }
+
+    header('Content-Type: application/json');
+    echo json_encode($data);
+
+    $con->close();
+?>
