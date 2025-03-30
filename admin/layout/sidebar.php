6b4257571830d1de9b8a00dcb376c4d281a0eac9
ngnhuan282 <nguyennhuan2003@gmail.com>
2025-03-30 21:25:13 +0700

    a

--- admin/layout/sidebar.php
+++ admin/layout/sidebar.php
@@ -1,12 +1,4 @@
-<div class="container">
-        <aside class="sidebar open">
-            <div class="top-sidebar">
-                <a href="#" class="channel-logo"><img src="../img/logo.png" alt="Lucy Coffe Logo"></a>
-                <div class="hidden-sidebar your-channel"><img src="../img/logo.png"
-                        style="height: 30px;" alt="">
-                </div>
-            </div>
-            <div class="middle-sidebar">
+<div class="middle-sidebar">
                 <ul class="sidebar-list">
                     <li class="sidebar-list-item tab-content active" data-mode="home">
                         <a href="#" class="sidebar-link">
@@ -58,33 +50,3 @@
                     </li>
                 </ul>
             </div>
-            <div class="bottom-sidebar">
-                <ul class="sidebar-list">
-                    <li class="sidebar-list-item user-logout">
-                        <a href="../index.html" class="sidebar-link">
-                            <div class="sidebar-icon"><i class="fa-solid fa-house"></i></div>
-                            <div class="hidden-sidebar">Trang chủ</div>
-                        </a>
-                    </li>
-                    <li class="sidebar-list-item user-logout">
-                        <a href="admin.html" class="sidebar-link">
-                            <div class="sidebar-icon"><i class="fa-light fa-circle-user"></i></div>
-                            <div class="hidden-sidebar" id="name-acc"><?php echo $_SESSION['username']; ?></div>
-                        </a>
-                    </li>
-                    <li class="sidebar-list-item user-logout">
-                        <a href="login.html" class="sidebar-link" id="logout-acc">
-                            <div class="sidebar-icon"><i class="fa-light fa-arrow-right-from-bracket"></i></div>
-                            <div class="hidden-sidebar">Đăng xuất</div>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </aside>
-        <main class="content">
-            <!-- Product  -->
-           
-            <!-- Account  -->
-            <!-- Order  -->
-        </main>
-    </div>
