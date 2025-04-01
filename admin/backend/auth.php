8f0b1b879410951a52d9cb399616dbc8edc6453d
ngnhuan282 <nguyennhuan2003@gmail.com>
2025-04-01 23:37:40 +0700

    Update auth.php

--- admin/backend/auth.php
+++ admin/backend/auth.php
@@ -14,25 +14,22 @@ class Auth {
     {
         $sql = "SELECT * FROM accounts WHERE username = '$username' AND password = '$password'";
         $result = mysqli_query($this->conn, $sql);
-        if($result)
+        if($result && mysqli_num_rows($result) > 0)
         {   
             $account = mysqli_fetch_assoc($result);
-            if($account)
-            {
-                $_SESSION['loggedin'] = true;
-                $_SESSION['username'] = $account['username'];
-                $_SESSION['id'] = $account['id'];
+            $_SESSION['loggedin'] = true;
+            $_SESSION['username'] = $account['username'];
+            $_SESSION['id'] = $account['id'];
     
-                $sql2 = "SELECT * FROM admin WHERE acc_id = ". $account['id'];
-                $result2 = mysqli_query($this->conn, $sql2);
-                if($result2)
-                {
-                    $admin = mysqli_fetch_assoc($result2);
-                    if($admin)
-                        $_SESSION['role'] = $admin['role'];
-                }
-                return true;
+            $sql2 = "SELECT * FROM admin WHERE acc_id = ". $account['id'];
+            $result2 = mysqli_query($this->conn, $sql2);
+            
+            if($result2 && mysqli_num_rows($result2) > 0)
+            {
+                $admin = mysqli_fetch_assoc($result2);
+                $_SESSION['role'] = $admin['role'];
             }
+            return true;
         }
         return false; //Đăng nhập thất bại;
     }
