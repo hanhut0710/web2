437e1ca34742cd2cbf5ea32e7b5adb42535a66a3
ngnhuan282 <nguyennhuan2003@gmail.com>
2025-04-01 23:53:24 +0700

    :<

---
+++ admin/backend/product.php
@@ -0,0 +1,25 @@
+<?php
+require_once "../config/database.php";
+class Product{
+    private $conn;
+
+    public function __construct()
+    {
+        $mysql = new Database();
+        $this->conn = $mysql -> getConnection();
+    }
+
+    public function getAllProduct()
+    {
+        $sql = "SELECT * FROM products";
+        $result = mysqli_query($this->conn, $sql);
+        $products = [];
+        if($result)
+        {
+            while($rows = mysqli_fetch_array($result))
+                $products[] = $rows;
+        }
+        return $products;
+    }
+}
+?>
