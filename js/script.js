4107e3b519558d9f6e9e5d48bd8dc8ac0226cd97
SLMR05-05 <jdurisk@gmail.com>
2025-04-11 16:07:34 +0700

    1

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ let currentCategory = 'all';
 let searchKeyword = '';
 let selectedBrand = '';
 let selectedPrice = '';
-const keyword = document.getElementById("searchInput2").value;
-const brand = document.getElementById("brandSelect").value;
-const priceRange = document.getElementById("priceSelect").value; // ví dụ: "1000000-3000000"
+// const keyword = document.getElementById("searchInput2").value;
+// const brand = document.getElementById("brandSelect").value;
+// const priceRange = document.getElementById("priceSelect").value; // ví dụ: "1000000-3000000"
 const [minPrice, maxPrice] = priceRange.split('-');
 
 
